852681a84275f494d3fbc3d63efd2aeabfc87bc4
Tony Yang <tianyuyang658@yahoo.com>
2017-02-12 14:40:27 -0800

    Add the images endpoint

--- falcon/look/app.py
+++ falcon/look/app.py
@@ -1,3 +1,7 @@
 import falcon
+import images
 
 api = application = falcon.API()
+
+images = images.Resource()
+api.add_route('/images', images)
---
+++ falcon/look/images.py
@@ -0,0 +1,6 @@
+import falcon
+
+class Resource(object):
+    def on_get(self, req, resp):
+        resp.body = '{"message": "Hello world!"}'
+        resp.status = falcon.HTTP_200
