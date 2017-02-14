b7e9d32cffb91aa793c7fa093d2c5ddf18a64814
Tony Yang <tianyuyang658@yahoo.com>
2017-02-13 18:46:14 -0800

    Add before hook to do content type checking for image

--- falcon/look/images.py
+++ falcon/look/images.py
@@ -4,10 +4,22 @@ import mimetypes
 
 import falcon
 
+ALLOWED_IMAGE_TYPES = (
+    'image/gif',
+    'image/jepg',
+    'image/png'
+)
+
+def validate_image_type(req, resp, resource, params):
+    if req.content_type not in ALLOWED_IMAGE_TYPES:
+        msg = 'Image type not allowed. Must be PNG, JPEG, or GIF'
+        raise falcon.HTTPBadRequest('Bad request', msg)
+
 class Collection(object):
     def __init__(self, storage_path):
         self.storage_path = storage_path
 
+    @falcon.before(validate_image_type)
     def on_post(self, req, resp):
         ext = mimetypes.guess_extension(req.content_type)
         filename = '{uuid}{ext}'.format(uuid=uuid.uuid4(), ext=ext)
