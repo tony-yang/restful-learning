ecce773ac2759b3286c581fb56678e63f3bf05f0
Tony Yang <tianyuyang658@yahoo.com>
2017-02-14 12:50:49 -0800

    Make sure view config and init route is correctly connected

--- pyramid/read/read/__init__.py
+++ pyramid/read/read/__init__.py
@@ -2,6 +2,6 @@ from pyramid.config import Configurator
 
 def main(global_config, **settings):
     config = Configurator(settings=settings)
-    config.add_route('strings', '/strings/{name}')
+    config.add_route('reads', '/strings/{name}')
     config.scan('.views')
     return config.make_wsgi_app()
