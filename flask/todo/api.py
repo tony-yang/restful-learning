794a4a1b29eb92719c52bb4aceb46f3f192b67f1
Tony Yang <tianyuyang658@yahoo.com>
2017-02-14 17:21:26 -0800

    Add a Todo demo in the same api file

--- flask/todo/api.py
+++ flask/todo/api.py
@@ -4,11 +4,23 @@ from flask_restful import Resource, Api
 app = Flask(__name__)
 api = Api(app)
 
+todos = {}
+
 class HelloWorld(Resource):
     def get(self):
         return {'hello': 'world'}
 
 api.add_resource(HelloWorld, '/')
+
+class TodoSimple(resource):
+    def get(self, todo_id):
+        return {todo_id: todos[todo_id]}
+
+    def put(self, todo_id):
+        todos[todo_id] = request.form['data']
+        return {todo_id: todos[todo_id]}
+
+api.add_resource(TodoSimple, '/<string:todo_id>')
 
 if __name__ == '__main__':
     app.run(debug=True)
